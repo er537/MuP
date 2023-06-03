955170bf1a2c6dc1f8c863aef0fbd5472fa5718e
er537 <elliereid98@outlook.com>
2023-06-03 16:14:54 +0100

    Add MuReadout to transformer

--- model/transforme_encoder.py
+++ model/transforme_encoder.py
@@ -5,17 +5,22 @@ from torch import nn, Tensor
 import torch.nn.functional as F
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 
+from mup import MuReadout
+
 class TransformerModel(nn.Module):
 
-    def __init__(self, ntoken: int, d_model: int, n_heads: int,
-                 nlayers: int, dropout: float = 1.0):
+    def __init__(self, n_tokens: int, d_model: int, n_heads: int,
+                 n_layers: int, dropout: float = 1.0, mup: bool = True):
         super().__init__()
         self.d_model = d_model
         self.pos_encoder = PositionalEncoding(d_model, dropout)
         encoder_layers = TransformerEncoderLayer(d_model, n_heads, d_model, dropout)
-        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
-        self.encoder = nn.Embedding(ntoken, d_model)
-        self.decoder = nn.Linear(d_model, ntoken)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, n_layers)
+        self.encoder = nn.Embedding(n_tokens, d_model)
+        if mup:
+            self.decoder = MuReadout(self.d_model, self.n_tokens, bias=False)
+        else:
+            self.decoder = nn.Linear(self.d_model, self.n_tokens, bias=False)
 
     def forward(self, x: Tensor) -> Tensor:
         """
@@ -23,7 +28,7 @@ class TransformerModel(nn.Module):
             x: Tensor, shape ``[seq_len, batch_size, feat_dim]``
 
         Returns:
-            output Tensor of shape ``[seq_len, batch_size, ntoken]``
+            output Tensor of shape ``[seq_len, batch_size, n_tokens]``
         """
         x = self.encoder(x) * self.d_model # MuP uses d_model not sqrt(d_model)
         x = self.pos_encoder(x)
